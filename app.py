056891f2ce6983b83353a43856fc6ed94b3a098b
StefanFVogel <58847673+StefanFVogel@users.noreply.github.com>
2025-03-03 10:43:57 +0100

    created a requirement.txt

--- app.py
+++ app.py
@@ -190,7 +190,7 @@ def delete(id):
     db.session.commit()
     return redirect(url_for('index'))
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+
+with app.app_context():
+    db.create_all()
+app.run(debug=True)
